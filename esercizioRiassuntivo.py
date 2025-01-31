6a8ff5ede9d54650359b0642d47b4f9a6c199a66
maxdl98 <delucamassimo9880@gmail.com>
2025-01-31 16:22:52 +0100

    Add files via upload
    modifica, ho inserito le altre cose del programma, e in base a ciò ho modificato un po la traccia

--- esercizioRiassuntivo.py
+++ esercizioRiassuntivo.py
@@ -7,7 +7,8 @@
 
 
 # Inizializzazione:
-
+#l'utente deve loggarsi
+# inserire username password e pin in un dizionario
 # Crea una variabile somma e impostala a 0. 
 # Questa variabile servirà per tenere traccia della somma dei numeri che l'utente inserisce.
 # Crea una variabile contatore e impostala a 0. 
@@ -15,8 +16,8 @@
 # Loop di inserimento:
 
 # Usa un ciclo while per chiedere all'utente di inserire un numero.
-# Se l'utente inserisce un numero negativo, 
-# il programma deve fermarsi e visualizzare il totale della somma dei numeri inseriti fino a quel momento e quanti numeri validi sono stati inseriti.
+# Se l'utente inserisce un numero negativo dispari, 
+# e aggiungerli a un set e stamparli.
 # Condizioni:
 # Se l'utente inserisce un numero pari, aggiungilo alla variabile somma e incrementa il contatore.
 # Se l'utente inserisce un numero dispari, 
@@ -25,21 +26,36 @@
 somma = 0
 cont = 0
 
-while True:
+username = "massi"
+password = "password123"
+pin = "1234"
+
+user_data = {
+    "username": "massi",
+    "password": "password123",
+    "pin": "1234"
+}
+
+user = input("Inserisci username: ")
+passwo = input("Inserisci password: ")
+pin_input = input("Inserisci il tuo pin: ")
+
+if user == user_data.get("username") and passwo == user_data.get("password") and pin_input == user_data.get("pin"):
     
-    chiedo = int(input("Inserisci un numero"))
-    
-    
+    while True:
+        chiedo = int(input("Inserisci un numero: "))
         
-    
-    if chiedo > 0 and chiedo % 2 == 0:
-        cont = cont + 1
-        somma = somma + chiedo
-    
-    if chiedo < 0 and chiedo % 2 == 1:
-        print("Hai inserito un numero non valido")
-        print("Ecco la somma: ", somma, "Ecco quanti numeri hai inserito: ", cont)
-        break
-
-
+        num = []  
+        messaggio = {"ciao inserisci numeri dispari"}  
+        
+        if chiedo > 0 and chiedo % 2 == 0:  
+            cont += 1
+            num.append(chiedo)
+            somma += chiedo
+        
+        elif chiedo < 0 and chiedo % 2 != 0: 
+            print("Hai inserito un numero non valido")
+            messaggio.add(chiedo)
+            print(messaggio)
+            break  
 
